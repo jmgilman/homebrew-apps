86eabd1df8985e35cbf9299bb6807975ecc61ba7
Joshua Gilman <joshuagilman@gmail.com>
2021-09-13 19:41:44 -0700

    Fixes vaulssh hashes

--- Formula/vaultssh.rb
+++ Formula/vaultssh.rb
@@ -8,11 +8,11 @@ class Vaultssh < Formula
 
   if OS.mac?
     url "https://github.com/jmgilman/vaultssh/releases/download/v0.1.0/x86_64-apple-darwin.tar.gz"
-    sha256 "66774b60d673fa91c9d8fd72497206f480ab517b7b3e3b72fd83bd4c368d261b"
+    sha256 "f9af5b6928c26a32e2671581c114126c35dc60b348c69cd63dd299fc79a09cc6"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/jmgilman/vaultssh/releases/download/v0.1.0/x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "69b3b688513822f2f1210c9b8ecc98a0905e8a4a8a4cc27c4995daa1595a8a05"
+      sha256 "c4776eea06a4b1f993838f7d82cdac44a92dc87a23d2a45f3dc8f3a1fd60afe9"
     end
   end
 
